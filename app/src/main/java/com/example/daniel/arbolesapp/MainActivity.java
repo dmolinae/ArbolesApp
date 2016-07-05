eb4536459d163be2e518e4b2b030ced243f010e3
Daniel Molina <dmolinae@outlook.com>
2016-07-05 05:02:14 -0400

    Apply initial style

--- app/src/main/java/com/example/daniel/arbolesapp/MainActivity.java
+++ app/src/main/java/com/example/daniel/arbolesapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.daniel.arbolesapp;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBar;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -40,6 +41,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Home");
+        actionBar.setSubtitle("Seleccione una opción");
     }
 
     @Override
@@ -80,17 +85,17 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_home) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_identify) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_glossary) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_forests) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_fav) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_about) {
 
         }
 
